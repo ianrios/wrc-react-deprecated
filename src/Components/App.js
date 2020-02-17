520a78aee4f45a09619663b2976e730ab3afebb6
ianrios <ian.rios1a@gmail.com>
2020-02-17 15:20:34 -0500

    added enter to main page and pictures and question mark cursor

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,6 +7,7 @@ import Q from './Q';
 import Navbar from './Navbar';
 import Route from './Routes';
 import Logo from "./Logo";
+import ScrollToTop from "./ScrollToTop";
 import './App.scss';
 
 function Wrapper() {
@@ -18,15 +19,26 @@ function Wrapper() {
 			setViewMain(false)
 		}
 	}, [pathArr]);
+	// const letters = "WHY? Record Company".split("").map(i => Math.floor(Math.random() * 2) === 1 ? "rotate" : "")
+	// console.log(letters)
+	// className={`${letters[k]}`}
+	const appTitle = "WHY? Record Company".split("").map((i, k) => i === "?" ? <Q size={2} key={k} /> : <span key={k} >{i}</span>)
 
 	return (
 		<div className="App">
 			{viewMain ?
 				<div className="body-grid">
-					<div className="main-image" onClick={() => setViewMain(!viewMain)}>
-						<div className='image-overlay-text'>
-							WHY<Q size={2} /> Record Company
-							</div>
+					<div className="main-image" style={{
+						backgroundImage: `url("/images/textures/${Math.floor(Math.random() * 6) + 1}.jpg")`
+					}} onClick={() => setViewMain(!viewMain)}>
+						<div className='image-overlay-text app-title'>
+							{appTitle}
+						</div>
+					</div>
+					<div className="enter-text">
+						<span className="questrial colored-link white-text" onClick={() => setViewMain(!viewMain)}>
+							enter
+						</span>
 					</div>
 				</div>
 				:
@@ -45,6 +57,7 @@ function Wrapper() {
 export default function App() {
 	return (
 		<BrowserRouter>
+			<ScrollToTop />
 			<Wrapper />
 		</BrowserRouter>
 	)
