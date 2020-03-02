ca8c67247273f85efff7babb768dd8884e653364
ianrios <ian.rios1a@gmail.com>
2020-03-02 08:45:58 -0500

    Updates

---
+++ precache-manifest.7efbb222e5690f212cb2481ff1bf8424.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a7ae950cf760e0b0842ac56641de9ec",
+    "url": "/wrc-react/index.html"
+  },
+  {
+    "revision": "4a7738910d65ad99c50d",
+    "url": "/wrc-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "9189031fde071c7ad9cb",
+    "url": "/wrc-react/static/css/main.c90c8301.chunk.css"
+  },
+  {
+    "revision": "4a7738910d65ad99c50d",
+    "url": "/wrc-react/static/js/2.6ef6b08d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/wrc-react/static/js/2.6ef6b08d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9189031fde071c7ad9cb",
+    "url": "/wrc-react/static/js/main.83fa11ad.chunk.js"
+  },
+  {
+    "revision": "d6a11704f15081a3f2fc",
+    "url": "/wrc-react/static/js/runtime-main.0f60124f.js"
+  },
+  {
+    "revision": "f73ac997d52af69ed4a22fad827b6d46",
+    "url": "/wrc-react/static/media/Lovelyn.f73ac997.ttf"
+  }
+]);
