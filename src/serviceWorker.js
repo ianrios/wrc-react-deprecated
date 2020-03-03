3fddea2ba6f22a7467c29c54a4b4ee8d1f0f57cd
ianrios <ian.rios1a@gmail.com>
2020-03-03 10:11:49 -0500

    modifying service worker code

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -32,7 +32,7 @@ export function register(config) {
 		}
 
 		window.addEventListener('load', () => {
-			const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+			const swUrl = `${process.env.PUBLIC_URL}/service-worker${(+new Date).toString(36)}.js`;
 
 			checkValidServiceWorker(swUrl, config);
 			if (isLocalhost) {
